90225e46606b7d7dccb2520bc8ad0ddccb6a7529
msKennedyJ <jk@brillantmont.ch>
2026-09-15 12:41:27 +0200

    Update randomWords.js

--- randomWords.js
+++ randomWords.js
@@ -1,4 +1,23 @@
-const words=["Elsie","Francisco","Gustaw","Alex","Dragos","Hakan"]
+const words=["Elsie",
+             "Francisco",
+             "Gustaw",
+             "Alex",
+             "Dragos",
+             "Hakan",
+             "Fransico",
+             "Andres",
+             "Chicken",
+             "Monkey",
+             "Badger",
+             "I hate your wonky face",
+             "Please go away, you make me feel sick",
+             "Were you dropped on your face as a child?",
+             "Are you part monkey?",
+             "What time is it?",
+             "Where did I leave my chicken",
+             "Please, not the face.",
+             "I have come to steal your SOUL",
+             "You remind me of a feral dog"]
 
 function generateRandomWord() {
   const randomIndex = Math.floor(Math.random()*words.length);
